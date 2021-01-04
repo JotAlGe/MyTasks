3d367c73014ce74ade7a5b9df484e258c4842590
Juanchis <juanchismo10@gmail.com>
2021-01-04 20:52:30 -0300

    animación del header en evento load

--- js/load.js
+++ js/load.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', () => {
     let main_form = document.getElementById('main-form')
+    let main_head = document.getElementById('main-head');
     main_form.classList.toggle('load')
+    main_head.classList.toggle('moveHead');
 })
