567aa600002d21c0b84972ecab30c86812952fa3
Juanchis <juanchismo10@gmail.com>
2021-01-05 10:27:32 -0300

    Correcciones en la validación del input y animación

--- js/add_task.js
+++ js/add_task.js
@@ -7,18 +7,16 @@ let but_remember = document.getElementById('but-remember')
 //but_remember.addEventListener('click', add_element)
 function validate(){
     let length = document.getElementById('main_text').value.length
+    let div_input = document.getElementById('div_input');
     
     if(length == 0 ){
-        let div_input = document.getElementById('div_input');
-            let paragraph = document.createElement('p')
-            paragraph.textContent = 'Do not leave the field empty, please!'
             div_input.classList.add('error')
-            div_input.firstElementChild.classList.add('error_input')
-            div_input.insertBefore(paragraph, circle.children[1])
+            div_input.children[0].classList.add('error_input')
+            div_input.children[0].placeholder = 'Do not leave the field empty, please!'
         }else{
             div_input.classList.remove('error')
             div_input.firstElementChild.classList.remove('error_input')
-            div_input.children[1].remove()
+            div_input.children[0].placeholder = 'Write a task or something to remember...'
         }
 
 
